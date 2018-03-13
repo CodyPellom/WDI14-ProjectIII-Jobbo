3d5ec94adec77e6be3e8485e3fceb7897db10709
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-12 20:28:31 -0400

    updated 830 pm

---
+++ Jobba/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express()
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', err => {
+    console.log(err)
+    })
+
+db.on('open', () => {
+console.log('Connected to MongoDB')
+})
+
+
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send("hello, wurld")
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Jobba is up and running on PORT ' + PORT)
+})
