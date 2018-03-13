77cfd98b20c49fc3c53c5721d050b845f565b948
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-13 11:57:03 -0400

    mongoDB connection & seeds

--- Jobba/db/seeds.js
+++ Jobba/db/seeds.js
@@ -1,3 +1,4 @@
+
 //Dependacies
 require('dotenv').config()
 const mongoose = require('mongoose');
@@ -7,4 +8,21 @@ const mongoose = require('mongoose');
 const CompaniesModel= require('./schema/Companies-model');
 const BioModel = require('./schema/Bio-model');
 const PositionsModel = require('./schema/Positions-model');
+
+
+
+//Connect to MongoDb Here:
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+//Here is the errors, or successful connections logger
+db.on('open', () => {
+    console.log('connected to mongoDB')
+})
+db.on('error', (err) => {
+    console.log(err)
+})
+
+
 
