fc979eee49df16ae097cffe6bcb6cdeee3c25d08
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-13 15:05:41 -0400

    getting routes set up in epress

--- Jobba/routes/Positions-Routes.js
+++ Jobba/routes/Positions-Routes.js
@@ -3,3 +3,14 @@ const router = express.Router()
 
 const { PositionsModel } = require('../db/schema/Positions-model')
 
+
+//index route:
+router.get('/', async (req, res) => {
+    try {
+        const PositionsModel = await PositionsAwait.find({})
+        res.json(PositionsModel)
+    } catch (err) {
+      console.log(err)
+    }
+})
+//show route:
